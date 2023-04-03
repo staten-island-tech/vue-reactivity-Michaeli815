3c620feaa6e405e5cac9cb012d7df0c3534a0334
Michaeli815 <113201056+Michaeli815@users.noreply.github.com>
2023-04-02 21:32:29 -0400

    klbhnijuy ,

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,9 @@
 <script>
-import card from "../components/card.vue";
+import CardItem from "./components/CardItem.vue";
 export default {};
 </script>
 
 <template>
   <h1>jbvvbbvvbng</h1>
-  <card />
+  <CardItem />
 </template>
---
+++ vite-project/src/components/CardItem.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "CardItem",
+};
+</script>
+
+<template>
+  <div>
+    <h1>gdsgss</h1>
+  </div>
+</template>
+
+<style>
+#card {
+  box-shadow: 10px 7px 5px rgb(255, 87, 87);
+  border-radius: 3rem;
+  width: 25%;
+  height: 43rem;
+  font-size: 7rem;
+  background-color: rgb(228, 37, 37);
+  margin: 8rem auto;
+  align-items: center;
+  border: 0.3rem solid rgb(107, 2, 28);
+  padding: 3rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+}
+</style>
