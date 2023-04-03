b3f8cce4906dc777a5bd5e07c8d45ae33e34973f
Michaeli815 <113201056+Michaeli815@users.noreply.github.com>
2023-04-02 21:14:18 -0400

    hg

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,9 +1,9 @@
 <script>
-export default{
-  
-}
+import card from "../components/card.vue";
+export default {};
 </script>
 
 <template>
   <h1>jbvvbbvvbng</h1>
+  <card />
 </template>
---
+++ vite-project/src/components/card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "card",
+};
+</script>
+
+<style>
+card {
+  .card {
+    box-shadow: 10px 7px 5px rgb(255, 87, 87);
+    border-radius: 3rem;
+    width: 25%;
+    height: 43rem;
+    font-size: 7rem;
+    background-color: rgb(228, 37, 37);
+    margin: 8rem auto;
+    align-items: center;
+    border: 0.3rem solid rgb(107, 2, 28);
+    padding: 3rem;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+  }
+}
+</style>
